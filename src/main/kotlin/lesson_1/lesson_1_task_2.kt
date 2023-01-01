c73b803f3e42b57a93b199029ee8406aaf4d9588
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-02 00:31:30 +0300

    KS-1-2 create and output three variables

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 80000
+    val thankText: String = "Thank you for choosing our product"
+    println("Number of orders: $numberOfOrders")
+    println("Thank text: $thankText")
+//    var numberOfWorkers = 2000
+//    println("Number of workers: $numberOfWorkers")
+    var numberOfWorkers = 1999
+    println("Number of workers: $numberOfWorkers")
+}
