c0430209cfd83e2db9f067f77cef4f03d43a2f2f
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-02 22:55:08 +0300

    KS-1-3 output date of Gagarin's flight each on a new line and landing time

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,11 @@
+package lesson_1
+    val year = 1967
+    var hour = 9
+    var minute = 7
+
+fun main() {
+    println("$year\n$hour\n$minute")
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
