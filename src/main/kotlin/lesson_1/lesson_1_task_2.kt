22b60a3b0b5d09969d4c91925759e8291379db22
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-03 17:43:29 +0300

    replace variables into fun body

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,9 +1,9 @@
 package lesson_1
+fun main() {
     var numberOfOrders: Int = 75
     val thankText: String = "Thank you for choosing our product"
     var numberOfWorkers = 2000
 
-fun main() {
     println("Number of orders: $numberOfOrders")
     println("Thank text: $thankText")
 //    println("Number of workers: $numberOfWorkers")
