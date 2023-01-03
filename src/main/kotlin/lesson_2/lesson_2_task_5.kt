299b3d124c868b183b06da9ce6b10fe183753cf9
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-03 20:49:13 +0300

    KS-2-5 print result after applying the compound formula

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -4,7 +4,7 @@ import kotlin.math.pow
 
 fun main() {
     val capital = 70_000
-    var interestRate = 0.167
+    val interestRate = 0.167
     val years = 20
 
     val result: Double = capital * (1+ interestRate).pow(years)
