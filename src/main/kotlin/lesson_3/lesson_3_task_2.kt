2305af1acaab6b446f86e255ebdaa452f4b7bcb2
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-03 23:28:16 +0300

    KS-3-2 print full name and ager 2 times

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+fun main() {
+    var name = "Татьяна"
+    var surname = "Андреева"
+    var middleName = "Сергеевна"
+    var age = 20
+
+    println("[$surname $name $middleName, $age]")
+    surname = "Сидорова"
+    age = 22
+    println("[$surname $name $middleName, $age]")
+}
