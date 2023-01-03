ce3a4970a4951055aa4808b3d5800231fb646aa0
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-03 21:56:18 +0300

    KS-3-1 print 2 variants of greetings

---
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_3
+
+fun main() {
+    var greetings = "Good afternoon, "
+    val userName = "Ivan"
+
+    println(greetings + userName)
+    greetings = "Good evening, "
+    println(greetings + userName)
+}
