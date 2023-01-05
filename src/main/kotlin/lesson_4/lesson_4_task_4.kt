07ea9f91311fba23d7b70295af8dc2903b7e86ca
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-05 13:56:44 +0300

    KS-5-1 changed val day to 5 and fix spaces in the println fun

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -1,14 +1,14 @@
 package lesson_4
 
 fun main() {
-    val day = 1
+    val day = 5
     val hands = day == 1
     val legs = day == 3
     val back = day == 5
     val abs = day == 1 || day == 3 || day ==5
 
-    println("Упражнения для рук: $hands\n" +
-            "Упражнения для ног: $legs\n" +
-            "Упражнения для спины: $back\n" +
+    println("Упражнения для рук:    $hands\n" +
+            "Упражнения для ног:    $legs\n" +
+            "Упражнения для спины:  $back\n" +
             "Упражнения для пресса: $abs")
 }
