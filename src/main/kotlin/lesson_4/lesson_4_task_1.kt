8b30f58bfe8312b34d97da82d3ea0313e505bfac
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-05 13:30:17 +0300

    KS-5-1 moved cons out of the fun main

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,10 +1,11 @@
 package lesson_4
 
+const val SEATS = 13
+
 fun main() {
-    val seats = 13
     val todaySeats = 13
     val tomorrowSeats = 9
 
-    println("Доступность столиков на сегодня: ${todaySeats < seats}\n" +
-            "Доступность столиков на завтра: ${tomorrowSeats < seats}")
+    println("Доступность столиков на сегодня: ${todaySeats < SEATS}\n" +
+            "Доступность столиков на завтра: ${tomorrowSeats < SEATS}")
 }
