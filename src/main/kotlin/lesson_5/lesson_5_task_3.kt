c901ef911d399b2710d7a51a36061f1e5cf1f594
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-05 17:49:48 +0300

    KS-5-3 result of the quiz

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,15 @@
+package lesson_5
+
+fun main() {
+    val randomNumbers = arrayOf((1..100).random(), (1..100).random())
+    println("Введите ваше первое число:")
+    val userNumber1 = readln().toInt()
+    println("Введите ваше второе число:")
+    val userNumber2 = readln().toInt()
+
+    if (randomNumbers.contains(userNumber1) && randomNumbers.contains(userNumber2))
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if (randomNumbers.contains(userNumber1) || randomNumbers.contains(userNumber2))
+        println("Вы выиграли утешительный приз!")
+    else println("Неудача! Крутите барабан!")
+}
