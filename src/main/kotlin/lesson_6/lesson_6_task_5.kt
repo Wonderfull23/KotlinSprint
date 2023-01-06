fb6219392870ee3dfe4f4ac99c27fa8b3105e340
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-06 16:53:59 +0300

    KS-6-5 replace while (true) with do-while

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -6,40 +6,36 @@ fun main() {
     println("Введите пароль для регистрации:")
     val userPassword = readln()
 
-    while (true) {
+    do {
         println("Введите логин для входа:")
-        if  (readln() != userLogin) {
-            println("Неверный логин")
-            continue
-        }
-        else {
-            println("Введите пароль для входа:")
-            if (readln() != userPassword) {
-                println("Неверный пароль")
-                continue
-            } else {
-                captcha()
-                break
-            }
-        }
-    }
+        val userInputLogin = readln()
+        if (userInputLogin == userLogin) break
+        println("Неверный логин")
+    } while (userInputLogin != userLogin)
+
+    do {
+        println("Введите пароль для входа:")
+        val userInputPassword = readln()
+        if (userInputPassword == userLogin) break
+        println("Неверный пароль")
+    } while (userInputPassword != userPassword)
+
+    captcha()
 }
 
-fun captcha(): Boolean {
+fun captcha() {
     var attempt = 3
 
-    while (attempt > 0) {
-        attempt--
+    while (attempt-- > 0) {
         val randomNumbers = Array<Int>(2) {(1..9).random()}
         println("Введите ответ: ${randomNumbers[0]} + ${randomNumbers[1]}")
         if (readln().toInt() == randomNumbers.sum()) {
             println("Добро пожаловать!")
-            return true
+            break
         } else {
-            if( attempt != 0) println("Неправильный ответ")
+            if (attempt != 0) println("Неправильный ответ")
             else println("Доступ запрещен")
             continue
         }
     }
-    return false
 }
