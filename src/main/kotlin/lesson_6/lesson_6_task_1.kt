3d2e74fae01eee59a40bc3ae4c5713895667b61f
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-06 16:37:27 +0300

    KS-6-1 add "break" to print success move after firs right input

--- src/main/kotlin/lesson_6/lesson_6_task_1.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -9,12 +9,14 @@ fun main() {
     do {
         println("Введите логин для входа:")
         val userInputLogin = readln()
+        if (userInputLogin == userLogin) break
         println("Неверный логин")
     } while (userInputLogin != userLogin)
 
     do {
         println("Введите пароль для входа:")
         val userInputPassword = readln()
+        if (userInputPassword == userPassword) break
         println("Неверный пароль")
     } while (userInputPassword != userPassword)
 
