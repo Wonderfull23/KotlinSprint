efc265a71876cd6d961c5eb5930685e9008f82e6
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-09 20:10:54 +0300

    KS-7-5 change the way of initialization String "chars"

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -2,13 +2,9 @@ package lesson_7
 
 fun main() {
     println("Enter password length please:")
+    var chars = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     val passwordLength = readln().toInt()
     var password = ""
-
-    var chars = ""
-    for (i in '0'..'9') chars += i
-    for (i in 'a'..'z') chars += i
-    for (i in 'A'..'Z') chars += i
 
     for (i in 0 until passwordLength) {
         password += chars.random()
