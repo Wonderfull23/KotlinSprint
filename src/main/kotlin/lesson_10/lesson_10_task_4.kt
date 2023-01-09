cc2a5559f274232fbd73bcfae450665d387f6bd6
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-09 21:39:09 +0300

    KS-10-4 create a simple roll dice game

---
+++ src/main/kotlin/lesson_10/lesson_10_task_4.kt
@@ -0,0 +1,28 @@
+package lesson_10
+
+fun main() {
+    var askUser = "Хотите бросить кости? Введите Да или Нет"
+
+    do {
+        println(askUser)
+        val userInput = readln()
+        if (userInput.lowercase() == "да") {
+            val userDice = throwTheDice()
+            println("Вы выбросили ${userDice[0]} и ${userDice[1]}")
+            val computerDice = throwTheDice()
+            println("Компьютер выбросил ${computerDice[0]} и ${computerDice[1]}")
+            compareDice(userDice, computerDice)
+            askUser = "Хотите бросить кости еще раз Введите Да или Нет"
+        }
+    } while (userInput.lowercase() == "да")
+}
+
+fun throwTheDice() = listOf((1..6).random(), (1..6).random())
+
+fun compareDice(userDice: List<Int>, computerDice: List<Int>) {
+    when {
+        userDice.sum() > computerDice.sum() -> println("Победило человечество")
+        userDice.sum() < computerDice.sum() -> println("Победила машина")
+        else -> println("Ничья")
+    }
+}
