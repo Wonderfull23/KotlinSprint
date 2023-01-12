767644365638d0deb8c93fb35c3dc14d836bc7c8
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-12 23:24:28 +0300

    KS-12-2 change class Day, put fields into primary constructor, print information about three obj of class Day()

--- src/main/kotlin/lesson_12/Day.kt
+++ src/main/kotlin/lesson_12/Day.kt
@@ -1,10 +1,15 @@
 package lesson_12
 
-class Day {
-    var dayTemperature: Int = 10
-    var nightTemperature: Int = 0
-    var isRain: Boolean = false
-    var atmospherePressure: Int = 750
+class Day(
+    _dayTemperature: Int = 10,
+    _nightTemperature: Int = 0,
+    _isRain: Boolean = false,
+    _atmospherePressure: Int = 750,
+) {
+    var dayTemperature = _dayTemperature
+    var nightTemperature = _nightTemperature
+    var isRain = _isRain
+    var atmospherePressure = _atmospherePressure
 
     override fun toString() = "Today is: day temperature: $dayTemperature°C, " +
             "night temperature: $nightTemperature°C, " +
