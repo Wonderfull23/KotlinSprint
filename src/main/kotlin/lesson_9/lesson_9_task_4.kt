bf09a7081a6c4682fdd25ab981899e8e698aa7a0
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-14 01:18:50 +0300

    KS-9-4 added space

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -1,7 +1,7 @@
 package lesson_9
 
 fun main() {
-    println("Enter 5 ingredients in one line separated by commas")
-    val ingredients = readln().split(",")
+    println("Enter 5 ingredients in one line separated by comma and space")
+    val ingredients = readln().split(", ")
     println("Your sorted list: ${ingredients.sorted().joinToString(", ")}")
 }
