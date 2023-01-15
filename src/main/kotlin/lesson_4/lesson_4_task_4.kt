3f97744274d6eb9cf09439a40339d5bf8ac1fbd2
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-15 13:48:39 +0300

    KS-4-4 add four variables Boolean type

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -1,14 +1,18 @@
 package lesson_4
 
 fun main() {
-    val day = 5
-    val hands = day == 1
-    val legs = day == 3
-    val back = day == 5
-    val abs = day == 1 || day == 3 || day ==5
-
-    println("Упражнения для рук:    $hands\n" +
-            "Упражнения для ног:    $legs\n" +
-            "Упражнения для спины:  $back\n" +
-            "Упражнения для пресса: $abs")
+    val numberOfDays = 5
+    val day = 2
+    val isEven = day % 2 == 0
+    val arms = !isEven
+    val abs = !isEven
+    val legs = isEven
+    val back = isEven
+    val message = """
+        Упражнения для рук:    $arms
+        Упражнения для ног:    $legs
+        Упражнения для спины:  $back
+        Упражнения для пресса: $abs
+    """.trimIndent()
+    println(message)
 }
