419b5d7719f99445f898017323407a81899675d3
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-22 23:11:30 +0300

    KS-17-1 created class with getter and setters fo variables

---
+++ src/main/kotlin/lesson_16/lesson_17_task_4.kt
@@ -0,0 +1,12 @@
+package lesson_16
+
+fun main() {
+    val quiz = Quiz("Standard question", "gdfgd")
+    println(quiz.answer)
+    quiz.question = "Do you want to start first?"
+    quiz.answer = "No"
+    println(quiz.answer)
+    quiz.question = "Do you want to start second?"
+    quiz.answer = "Yes"
+    println(quiz.answer)
+}
