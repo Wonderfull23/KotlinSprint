f8676392e2cf13c6f1909e90dd12ebdfcc43918b
Wonderfull23 <mywonderkotlin23@gmail.com>
2023-01-22 03:08:57 +0300

    KS-16-5 created class with private valuable

---
+++ src/main/kotlin/lesson_16/leson_16_task_5.kt
@@ -0,0 +1,11 @@
+package lesson_16
+
+fun main() {
+    val player = Player("Gerald")
+    player.getDamage(10)
+    player.heal(30)
+    player.getDamage(80)
+    player.heal(40)
+    player.getDamage(100)
+    player.heal(10)
+}
